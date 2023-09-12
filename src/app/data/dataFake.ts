260cb60836c9a4c93764b7801f1d9a08d18881e4
Mateus Virgílio <virgilionr15@gmail.com>
2023-09-12 16:31:56 -0300

    responsive

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -8,8 +8,21 @@ export const dataFake = [
     {
         "id":"2",
         "title":"Novo Ipad lançado!",
-        "description": "blabla",
+        "description": "Praticidade e tecnologia acima do valor",
         "photoCover": "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/ipad-mini-finish-select-gallery-202211-pink-wifi_AV1_FMT_WHH?wid=1280&hei=720&fmt=p-jpg&qlt=95&.v=1670631638622"
+    },
+    {
+        "id":"3",
+        "title":"Conheça os poderes do JavaScript.",
+        "description": "Uma das linguagens de programação mais usadas no mundo. Pode ser utilizada tanto no front-end quanto no back-end.",
+        "photoCover": "https://www.resilia.com.br/wp-content/uploads/2022/07/1_6ahbWjp_g9hqhaTDSJOL1Q.png"
+    },
+    {
+        "id":"4",
+        "title":"Mergulhe no mundo da programação!",
+        "description": "O setor que mais cresce mesmo em épocas de crise.",
+        "photoCover": "https://www.supero.com.br/wp-content/uploads/2021/09/escolher-linguagens-de-programa%C3%A7%C3%A3o-em-baixa.jpg"
     }
+
 
 ]
