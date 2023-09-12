7dcb62b6fb3a085183e462a38bd50c2580b8f094
Mateus Virgílio <virgilionr15@gmail.com>
2023-09-12 15:39:14 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,15 @@
+export const dataFake = [
+    {
+        "id":"1",
+        "title":"Novo notebook da Dell é lançado!",
+        "description": "Designer moderno e desempenho acima da média",
+        "photoCover": "https://i.dell.com/is/image/DellContent//content/dam/ss2/product-images/dell-client-products/notebooks/latitude-notebooks/14-3420/global-spi/ng/notebook-latitude-14-3420-campaign-hero-504x350-ng.psd?fmt=jpg&wid=504&hei=334"
+    },
+    {
+        "id":"2",
+        "title":"Novo Ipad lançado!",
+        "description": "blabla",
+        "photoCover": "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/ipad-mini-finish-select-gallery-202211-pink-wifi_AV1_FMT_WHH?wid=1280&hei=720&fmt=p-jpg&qlt=95&.v=1670631638622"
+    }
+
+]
